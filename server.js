88fa76c0a36e5953c592b942ffdd40a856c83dc4
chefbrams <oldcitycatering@gmail.com>
2020-12-13 01:56:56 -0500

    removed folder

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 const path = require("path");
-const db = require("./Develop/models");
+const db = require("./models");
 const PORT = process.env.PORT || 3000;
 
 
@@ -25,14 +25,14 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
 
 
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname + "/Develop/public/index.html"));
+  res.sendFile(path.join(__dirname + "/public/index.html"));
 });
 app.get("/exercise", (req, res) => {
-  res.sendFile(path.join(__dirname +  "/Develop/public/exercise.html"));
+  res.sendFile(path.join(__dirname +  "/public/exercise.html"));
 });
 
 app.get("/stats", (req, res) => {
-  res.sendFile(path.join(__dirname +  "/Develop/public/stats.html"));
+  res.sendFile(path.join(__dirname +  "/public/stats.html"));
 });
 
 //creates new workout
